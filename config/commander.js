461b7788f75f0b033db9e70f134f5d82de123c48
kenxdrgn <ken0ryu@gmail.com>
2022-07-19 21:03:54 +0900

    refactor codes

--- config/commander.js
+++ config/commander.js
@@ -4,10 +4,7 @@ const program = new Command();
 
 program.name('app.js').description('CLI to NFT sales bot').version('0.1.0');
 
-program.option(
-	'-t, --test <tx>',
-	'test by running the app for the given transaction hash'
-);
+program.option('-t, --test <tx>', 'test by running the app for the given transaction hash');
 
 program.parse();
 
