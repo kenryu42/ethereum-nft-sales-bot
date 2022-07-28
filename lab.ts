65f74bcf700175bf182134a2de7b649011462062
kenxdrgn <ken0ryu@gmail.com>
2022-07-29 00:27:16 +0900

    removed unuse file

--- lab.ts
+++
@@ -1,8 +0,0 @@
-import { ethers } from 'ethers';
-
-const a = ethers.BigNumber.from('16000000000000000000');
-const b = ethers.BigNumber.from('400000000000000000');
-const c = a.sub(b);
-const d = ethers.utils.formatEther(c.toString());
-
-console.log(d);
