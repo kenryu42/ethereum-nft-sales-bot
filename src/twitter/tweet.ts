dc801f1e5c8e16c0cce8fe68b17565a4b041dcc3
Sterling Crispin <sterlingcrispin@gmail.com>
2022-08-19 08:04:27 -0700

    fixing sweep bug

--- src/twitter/tweet.ts
+++ src/twitter/tweet.ts
@@ -37,7 +37,7 @@ const tweet = async (tx: TransactionData) => {
     if (tx.isSwap && tx.addressMaker && tx.addressTaker && !DISCORD_ENABLED) {
         tx.gifImage = await createSwapGif(tx.swap, tx.addressMaker, tx.addressTaker);
     } else if (
-        (tx.isSweep || (GIF_ENABLED && (tx.quantity ?? 0) > 1 && tx.tokenType === 'ERC721')) &&
+        ((GIF_ENABLED && (tx.quantity ?? 0) > 1 && tx.tokenType === 'ERC721')) &&
         !DISCORD_ENABLED
     ) {
         tx.gifImage = await createGif(tx.tokens, tx.contractAddress, tx.tokenType);
