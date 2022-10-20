5d718213b56045545b8265d7d2dc894fb1063c29
p7m <perpetuum7@protonmail.com>
2022-10-20 19:46:38 +0200

    replace kodex disk with wizard

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -117,7 +117,7 @@ const getKodexName = async (address: string) => {
 
         if (!username) return null;
 
-        return `${username} @ 💾`;
+        return `${username} 🧙‍♂️`;
     } catch (error) {
         console.log('getKodexName API error');
         console.log(`address: ${address}`);
