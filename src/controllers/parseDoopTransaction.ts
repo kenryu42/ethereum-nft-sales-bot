88d5806bd35dd37b42a4447f91894fa53189f527
Brody Berson <2766432+mynamebrody@users.noreply.github.com>
2023-02-02 17:09:11 -0500

    Linting

--- src/controllers/parseDoopTransaction.ts
+++ src/controllers/parseDoopTransaction.ts
@@ -5,34 +5,26 @@ import { parseDoopMarket } from './parseDoopMarket.js';
 import { initializeDoopData } from '../config/initializeDoop.js';
 import { alchemy } from '../config/setup.js';
 
-async function parseDoopTransaction(
-    transactionHash: string,
-    contractAddress: string
-) {
+async function parseDoopTransaction(transactionHash: string, contractAddress: string) {
     const receipt = await alchemy.core.getTransactionReceipt(transactionHash);
     const recipient = receipt ? receipt.to.toLowerCase() : '';
     console.log(recipient);
-    const tx = initializeDoopData(
-        transactionHash,
-        recipient,
-        contractAddress
-    );
+    const tx = initializeDoopData(transactionHash, recipient, contractAddress);
 
     if (!receipt || !(recipient in doops)) {
         return null;
     }
-   
 
     for (const log of receipt.logs) {
         const logAddress = log.address.toLowerCase();
         const logDoop = _.get(doops, logAddress);
-        
-        if (logDoop.name === 'dooplicator'){
+
+        if (logDoop.name === 'dooplicator') {
             const parseResult = await parseDooplicate(tx);
             console.log(tx);
             if (parseResult === null) return null;
         }
-        if (logDoop.name === 'dooplicator-marketplace'){
+        if (logDoop.name === 'dooplicator-marketplace') {
             const parseResult = await parseDoopMarket(tx);
 
             if (parseResult === null) return null;
